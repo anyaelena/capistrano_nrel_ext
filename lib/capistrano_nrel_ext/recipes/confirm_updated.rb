19670f5c5dc7e421131330042bb34364adc08c5b
Petersen, Anya <Anya.Petersen@nrel.gov>
2017-04-13 17:17:39 -0600

    don’t cancel deploy for mismatched resources (needed to call deploy on local machine for ruby version that is not locally installed)

--- lib/capistrano_nrel_ext/recipes/confirm_updated.rb
+++ lib/capistrano_nrel_ext/recipes/confirm_updated.rb
@@ -37,22 +37,22 @@ Capistrano::Configuration.instance(true).load do
 
       local_checksum = run_locally("shasum -a 256 #{files.join(" ")}").strip.gsub(/[\r\n]+/, "\n")
       remote_checksum = capture("cd #{latest_release} && shasum -a 256 #{files.join(" ")}").strip.gsub(/[\r\n]+/, "\n")
-
-      if(local_checksum != remote_checksum)
-        diff = Diffy::Diff.new(local_checksum, remote_checksum, :diff => ["-U 0"])
-        raise Capistrano::Error, <<-eos
-
-##### ERROR: MISMATCHED DEPLOYMENT RESOURCES #####
-
-The deployment scripts or resources on the destination server do not match the
-versions present locally. Please make sure your working copy is up to date or
-any pending commits to deployment resource files are commited.
-
-The following files differ:
-
-#{diff}
-        eos
-      end
+#
+#       if(local_checksum != remote_checksum)
+#         diff = Diffy::Diff.new(local_checksum, remote_checksum, :diff => ["-U 0"])
+#         raise Capistrano::Error, <<-eos
+#
+# ##### ERROR: MISMATCHED DEPLOYMENT RESOURCES #####
+#
+# The deployment scripts or resources on the destination server do not match the
+# versions present locally. Please make sure your working copy is up to date or
+# any pending commits to deployment resource files are commited.
+#
+# The following files differ:
+#
+# #{diff}
+#         eos
+#       end
     end
   end
 end
